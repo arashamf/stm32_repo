c5ac830b87508e7c56890b121981b6ef1537974d
arashamf <arashamph@gmail.com>
2023-04-07 16:16:05 +0300

    delete timer jobs

--- timer/timer_delay/DELAY_TIM7.h
+++
@@ -1,5 +0,0 @@
-#ifndef DELAY_TIM7 // если не определен
-#define DELAY_TIM7
-void delay_us(unsigned int us); //прототип ф-ии задержки в мкс
-void delay_ms(unsigned int ms); //прототип ф-ии задержки в мс
-#endif
